03f97afdfece343d90db4c0515a03a872dc57a48
Jose Ramon <joseoviedo.do@gmail.com>
2013-11-14 13:46:46 -0800

    Limpiando un poco

--- core/lib.php
+++ core/lib.php
@@ -1,12 +1,15 @@
 <?php
     require_once('core/active.php');
+	
 	/**
-	* Archivo para almacenar las librerias y funcion utilitarias en la aplicacion
-	* Autor: AngelKurten
-	*/
+	 * Archivo para almacenar las librerias y funcion utilitarias en la aplicacion
+	 * Autor: AngelKurten
+	 * 
+	 */
 
 	//Funcion para personalizar los errores de PHP
-	function showErrors ($numero, $error, $archivo, $linea) {
+	function showErrors ($numero, $error, $archivo, $linea)
+	{
 		echo '<br/>Ha ocurrido un error en el archivo ' . $archivo . ', la linea ' . $linea . '<br/> ERROR: ' . $error . '';
 	}
 	set_error_handler('showErrors');
@@ -18,33 +21,32 @@
 		try 
 		{
 			//detectando si es un string
-			if (gettype($lib)=='string') {
+			if (gettype($lib) == 'string') {
 				//importando y verificando si se importo la libreria
 				if (!include_once($lib)) {
 					//enviando una exepcion con un mensaje de error
-					throw new Exception("Ha ocurrido un error a la hora de imprtar la libreria ".$lib);
+					throw new Exception('Ha ocurrido un error a la hora de imprtar la libreria ' . $lib);
 				}
 			}
 			//detectando si es un array
-			elseif (gettype($lib)=='array') {
+			elseif (gettype($lib) == 'array') {
 				//recorriendo el array
 				foreach ($lib as $key => $value) {
 					//detectando si es un string
-					if (gettype($value)!=='string') {
+					if (gettype($value) !== 'string') {
 						//enviando una exepcion con un mensaje de error
-						throw new Exception("Estas tratando	 de importar un archivo incorrecto ".$value);
+						throw new Exception('Estas tratando de importar un archivo incorrecto ' . $value);
 					}
 					//importando y verificando si se importo la libreria
-					elseif(!include_once($value)) {
+					elseif (!include_once($value)) {
 						//enviando una exepcion con un mensaje de error
-						throw new Exception("Ha ocurrido un error a la hora de importar la libreria ".$value);
+						throw new Exception('Ha ocurrido un error a la hora de importar la libreria ' . $value);
 					}
 				}
 			}
-			else
-			{
+			else {
 				//si el tipo de dato no es array y tampoco string envia un error
-				throw new Exception("Estas tratando	 de importar un archivo incorrecto ".$value);
+				throw new Exception('Estas tratando de importar un archivo incorrecto ' . $value);
 			}			
 		} 
 		catch (Exception $e)
@@ -60,5 +62,3 @@
 	    list($usec, $sec) = explode(" ", microtime());
 	    return ((float)$usec + (float)$sec);
 	}
-
-?>
