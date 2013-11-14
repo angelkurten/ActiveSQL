3d4b225eaabc819ce7b43ba50386bba196a1c17e
Jose Ramon <joseoviedo.do@gmail.com>
2013-11-14 13:22:21 -0800

    Limpiando un poco
    Aplicando un poco de lógica y de estándares.

--- core/active.php
+++ core/active.php
@@ -3,57 +3,58 @@
 	class active extends db
 	{
 		//variable para almacenar la consulta del where
-		private $where='';
+		private $where = '';
+		
 		//varaiable para almacenar los camos a filtrar en el GET
-		private $select='';
+		private $select = '';
+		
 		//variables para almacenar los campos a actualizar en el SET
-		private $set='';
+		private $set = '';
+		
 		//variable para los values del INSERT
-		private $values='';
+		private $values = '';
+		
 		//variable para limitar una consulta
-		private $limit='';
+		private $limit = '';
 		
 		public function __construct()
 		{
-			$this->mensaje='';
+			$this->mensaje = '';
 		}
 
 		//function para realizar una consulta de tipo SELECT
 		public function get($table)
-		{		
-			if($this->mensaje==''){
+		{
+			$result = $this->mensaje;
+			
+			if (!$result) {
+				
 				//construir consulta con o sin campos a filtrar
-				if($this->select!=''){
-					$this->query='SELECT '.$this->select.' FROM '.$table;
-				}
-				else
-				{
-					$this->query='SELECT * FROM '.$table;
+				$this->query = 'SELECT * FROM ' . $table;
+				
+				if ($this->select != '') {
+					$this->query = 'SELECT ' . $this->select . ' FROM ' . $table;
 				}
 
 				//completar consulta con o sin filtro where
-				if($this->where!='')
-				{
-					$this->query.=' WHERE '.$this->where;
+				if ($this->where != '') {
+					$this->query .= ' WHERE ' . $this->where;
 				}
 
 				//limitar la consulta
-				if($this->limit!='')
-				{
-					$this->query.= $this->limit;
+				if ($this->limit != '') {
+					$this->query .= $this->limit;
 				}
 
 				//ejecutar consulta
 				$this->getQuery();
 
-				$result=$this->rows;
+				$result = $this->rows;
 			}
-			else
-			{
-				$result=$this->mensaje;
-			}
+			
 			//liberar memoria
 			$this->liberar();
+			
 			//retornar el resultado
 			return $result;
 		}
@@ -61,21 +62,21 @@
 		//funcion para guardar datos en una BD
 		public function save($table)
 		{
-			if($this->mensaje=='')
-			{
-				$this->query='INSERT INTO '.$table.' VALUES '.$this->values.';';
+			$result = $this->mensaje;
+			
+			if (!$result) {
+				
+				$this->query = 'INSERT INTO ' . $table . ' VALUES ' . $this->values . ';';
 				
 				//ejecutar consulta
 				$this->executeQuery();
 
-				$result=$this->rows;
+				$result = $this->rows;
 			}
-			else
-			{
-				$result=$this->mensaje;
-			}
+		
 			//liberar la memoria
 			$this->liberar();
+			
 			return $result;
 		}
 
@@ -83,25 +84,21 @@
 		//funcion apra realizar actualizaciones en la BD
 		public function edit($table)
 		{
-			if($this->mensaje=='')
-			{
+			$result = $this->mensaje;
+			
+			if (!$result) {
 				//construir consulta con o sin where
-				if($this->set!='')
-				{
-					$this->query='UPDATE '.$table.' SET '.$this->set;
+				if ($this->set != '') {
+					$this->query = 'UPDATE ' . $table . ' SET ' . $this->set;
 				}
 				
-				if($this->where!='')
-				{
-					$this->query.=' WHERE '.$this->where;
+				if ($this->where != '') {
+					$this->query .= ' WHERE ' . $this->where;
 				}
 
-				$result=$this->executeQuery();
+				$result = $this->executeQuery();
 			}
-			else
-			{
-				$result=$this->mensaje;
-			}
+			
 			//liberar la memoria
 			$this->liberar();
 			//retornar el resultado
@@ -111,11 +108,11 @@
 		//function para borrar campos de la BD
 		public function delete($table)
 		{
-			if($this->mensaje=='')
-			{
-				$this->query='DELETE FROM '.$table;
-				if ($this->where!='') {
-					$this->query.=' WHERE '.$this->where;
+			if (!$this->mensaje) {
+				$this->query = 'DELETE FROM '.$table;
+				
+				if ($this->where != '') {
+					$this->query .= ' WHERE '.$this->where;
 				}
 
 				$this->executeQuery();
@@ -127,68 +124,73 @@
 		public function set(array $array)
 		{
 			//extraer las keys del array
-			$columnas=array_keys($array);
+			$columnas = array_keys($array);
+			
 			//extraer los values del array
-			$values=array_values($array);
+			$values = array_values($array);
+			
 			//crear el array definitivo
-			for ($i=0; $i < count($values); $i++) { 
-				$sql[$i]="".$columnas[$i]." = '".addslashes($values[$i])."'";
+			for ($i = 0; $i < count($values); $i++) { 
+				$sql[$i] = "" . $columnas[$i] . " = '" . addslashes($values[$i]) . "'";
 			}
 			//crear el sql definitivo 
-			$sqldf=implode(" and ", $sql);
+			$sqldf = implode(" and ", $sql);
 
 			//almaceno el sql en la propiedad
-			$this->set=$sqldf;
+			$this->set = $sqldf;
 		}
 
 		//funcion para seleccionar campos de una tabla
 		public function select(array $array)
 		{
-			$values=implode(" , ", $array);
-			$this->select=$values;
+			$values = implode(" , ", $array);
+			$this->select = $values;
 		}
 
 		//funcion para seleccionar los values
 		public function values(array $array)
 		{
 
-			$values=array_values($array);
+			$values = array_values($array);
+			
 			//crear un array definitivo con los arrays anteriores
-			for ($i=0; $i < count($values); $i++) {
-				$sql[$i]="'".addslashes($values[$i])."'";
+			for ($i = 0; $i < count($values); $i++) {
+				$sql[$i] = "'" . addslashes($values[$i]) . "'";
 			}
 
-			$values="(".implode(" , ", $sql).")";
+			$values = "(" . implode(" , ", $sql) . ")";
 
-			$this->values=$values;
+			$this->values = $values;
 		}
 
 		//funcion para realizar consultas tipo where
 		public function where( array $array)
 		{
 			//extraer las keys del array
-			$columnas=array_keys($array);
+			$columnas = array_keys($array);
+			
 			//extraer todos los values del array
-			$values=array_values($array);
+			$values = array_values($array);
+			
 			//crear un array definitivo con los arrays anteriores
-			for ($i=0; $i < count($values); $i++) { 
-				$sql[$i]="".$columnas[$i]." = '".addslashes($values[$i])."'";
+			for ($i = 0; $i < count($values); $i++) { 
+				$sql[$i] = "" . $columnas[$i] . " = '" . addslashes($values[$i]) . "'";
 			}
+			
 			//creo el sql definitivo
-			$sqldf=implode(" and ", $sql);
+			$sqldf = implode(" and ", $sql);
 
-			$this->where=$sqldf;
+			$this->where = $sqldf;
 		}
 
 		//funcion para limitar resultados de una consulta
-		public function limit($init, $end=NULL)
+		public function limit($init, $end = NULL)
 		{
 			try {
-				if ((is_int($init)) and ($init>-1)) {
-					$this->limit=' LIMIT '.$init;
-					if(($end!=NULL) and (is_int($end)) and ($end > -1))
-					{
-						$this->limit.=', '.$end;
+				if ((is_int($init)) and ($init > -1)) {
+					$this->limit = ' LIMIT ' . $init;
+					if (($end != NULL) and (is_int($end)) and ($end > -1)) {
+						$this->limit .= ', ' . $end;
 					}
 				}
 			} catch (Exception $e) {
@@ -199,12 +201,12 @@
 		//funcion para liberar memoria
 		public function liberar()
 		{
-			$this->where="";
-			$this->select="";
-			$this->set="";
-			$this->values="";
-			$this->mensaje="";
-			$this->limit="";
-			$this->rows=array();
+			$this->where = '';
+			$this->select = '';
+			$this->set = '';
+			$this->values = '';
+			$this->mensaje = '';
+			$this->limit = '';
+			$this->rows = array();
 		}
 	}
