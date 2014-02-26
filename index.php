f71c5c6a168465df3b29f1c5ff10000aa50babe5
Angel Kurten <angelkurten@hotmail.com>
2014-02-26 11:51:07 -0500

    v.0.1.1.3
    Modify index

--- index.php
+++ index.php
@@ -16,19 +16,20 @@
 	$time_start = microtime_float();
 
 		//consultar datos de la BD-------------------------
-		$array=array('Cod_Dpto'=>1);
+		//$array=array('Cod_Dpto'=>1);
 		//envio a la RAM los campos a filtrar
-		$ac->where($array);
+		//$ac->where($array);
 		//$ac->groupBy(array('apellido'));
 		$ac->limit(10);
 		//array con los campos a seleccionar
 		$array= array('*');		
 		//envio a la RAM los campos a seleccionar
 		$ac->select($array);
-		$ac->join('departamentos','departamentos.Cod_Dpto=ciudades.Cod_Dpto');
+		$ac->join('departamentos','departamentos.Cod_Dpto=personas.Cod_Dpto');
+		$ac->join('ciudades','ciudades.Cod_Ciudad=personas.Cod_Ciudad');
 		//recupero los datos de la RAM, ejecuto la consulta y 
 		//limpio la RAM
-		$user=$ac->get('ciudades')->json();
+		$user=$ac->get('personas')->object();
 		
 	//finalizar el tiempo de ejecucion
 	$time_end = microtime_float();
@@ -37,7 +38,7 @@
 	//mostrar el tiempo de ejecuion
 	echo "Se han consultado ".count($user)." registros en $time_total segundos";
 	echo "<br>";
-	echo $user;
+	var_dump($user);
 ?>
 
 </body>
